d59be698b5ab5b6c2c65cbe190e926616b579ba7
Ahmed.M.Ibrahim <35659429+am305949@users.noreply.github.com>
2021-01-25 06:54:07 +0200

    fix accuracy problem

--- PyloXyloto/net.py
+++ PyloXyloto/net.py
@@ -26,16 +26,12 @@ class Network:
 
         # training loop
         for i in range(epochs):
-            err = 0
-            out_list, predicted = [], []
+            err, acc = 0, 0
             for j in range(samples):
                 # forward propagation
                 output = x_train[j]
                 for layer in self.layers:
                     output = layer.forward_propagation(output)
-                    out_list = list(output[0])
-
-                predicted.append(out_list)
 
                 # compute loss
                 err += self.loss(y_train[j], output, x_train[j])
@@ -45,49 +41,24 @@ class Network:
                 for layer in reversed(self.layers):
                     error = layer.backward_propagation(error, learning_rate)
 
-                pred = identity_decoder(output[0])
-                label = identity_decoder(y_train[j])
-
                 pred = output[0]
                 label = y_train[j]
 
-                # print(np.array(label))
-                # print(np.array(label)[0])
-                # print(1 - np.array(label))
-                # print(np.array(pred))
-                # print(1 - np.array(pred))
-                # print(np.array(label).shape)
-                # print(np.array(pred).shape)
-                # print(np.array(label).size)
-
-                # calculate accuracy for each sample
-                accuracy = float((np.dot(np.array(label), np.array(pred).T) +
-                                  np.dot(1 - np.array(label), 1 - np.array(pred).T)) / float(
-                    np.array(label).size) * 100)
+                # calculate average accuracy for each sample
+                acc += float(
+                    (np.dot(np.array(label), np.array(pred).T) + np.dot(1 - np.array(label), 1 - np.array(pred).T))
+                    / float(np.array(label).size) * 100)
 
             # calculate average error and average accuracy on all samples
-            # pred = identity_decoder(predicted)
-            # label = identity_decoder(y_train)
 
-            # print(np.array(label))
-            # print(np.array(label)[0])
-            # print(1 - np.array(label))
-            # print(np.array(pred))
-            # print(1 - np.array(pred))
-            # print(np.array(label)[0].shape)
-            # print(np.array(pred)[0].shape)
-            # print(np.array(label).size)
-
-            # accuracy = float((np.dot(np.array(label)[0], np.array(pred)[0].T) +
-            #                   np.dot(1-np.array(label)[0], 1-np.array(pred)[0].T)) / float(np.array(label)[0].size)*100)
-
-            # accuracy /= samples
             err /= samples
+            acc /= samples
 
             self.error_per_epoch.append(err)
-            self.accuracy_per_epoch.append(accuracy)
+            self.accuracy_per_epoch.append(acc)
 
-            print('error={:.9f}         accuracy={:.9f}         for epoch {} of {}'.format(err, accuracy, i+1, epochs))
+            print(
+                'error={:.9f}         accuracy={:.9f}         for epoch {} of {}'.format(err, acc, i + 1, epochs))
             # visualize([k for k in range(i+1)], self.error_per_epoch)
 
         return self.error_per_epoch, self.accuracy_per_epoch
@@ -107,6 +78,3 @@ class Network:
             result.append(out_list)
 
         return np.array(result)
-
-
-
